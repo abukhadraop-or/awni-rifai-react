b6faa43aa17d56e132d1b19f01166eec0869ad82
Awni-Alrifai <Awni.Rifai@optimumpartners.co>
2022-04-12 15:34:11 +0300

    remove clickOutside test

--- src/components/MovieCard/MovieCard.test.jsx
+++ src/components/MovieCard/MovieCard.test.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
 import MovieCard from 'components/MovieCard/MovieCard';
 
 const mockedData = {};
-
-function MockCardsContainer() {
-  return (
-    <>
-      <div>Click outside</div>
-      <MovieCard data={mockedData} />
-    </>
-  );
-}
-
 describe('CardMovie', () => {
   it('should display more list when more btn is clicked', async () => {
     render(<MovieCard data={mockedData} />);
@@ -22,16 +12,5 @@ describe('CardMovie', () => {
     const tooltipElement = screen.getByRole('tooltip');
     expect(tooltipElement).toBeVisible();
   });
-  it('should hide more list when clicking outside the component', async () => {
-    render(<MockCardsContainer />);
-
-    const clickOutSideContainer = screen.getByText('Click outside');
-    const button = screen.getByRole('button');
-
-    fireEvent.click(button);
-    fireEvent.click(clickOutSideContainer);
-    const tooltipElement = await screen.findByRole('tooltip');
-    expect(tooltipElement).not.toBeVisible();
-
-  });
+ 
 });
